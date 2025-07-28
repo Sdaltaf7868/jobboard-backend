f5db0d04163768b75b6c75b65c834d57e9d0f122
Shaik Altaf <sdaltaf7868@gmail.com>
2025-07-28 16:01:55 +0530

    Add root route for Railway health check

--- first.js
+++ first.js
@@ -1,4 +1,17 @@
-const app = require('./app');
-const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jobs = require('./jobsdata'); // 
+app.use(express.json());
+app.use(cors()); // You can replace '*' with your frontend URL in production
 
-module.exports.handler = serverless(app);
+app.get('/api/jobs', (req, res) => {
+  res.json(jobs);
+});
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8000}`);
+}); 
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
