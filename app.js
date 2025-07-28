526b4ad02e811d31130f6b0dd894617239e3467b
Shaik Altaf <sdaltaf7868@gmail.com>
2025-07-28 13:54:10 +0530

    Fix: Updated backend for Vercel serverless

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jobs = require('./jobsdata'); // 
-const mongoose = require('mongoose');
 app.use(express.json());
 app.use(cors()); // You can replace '*' with your frontend URL in production
 
 app.get('/api/jobs', (req, res) => {
   res.json(jobs);
 });
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 8000}`);
-}); 
+
+
+module.exports = app; // Export the app for testing or further configuration
