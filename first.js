5c9b86c45476e64c85fbe9a16277f03a43e8fd1f
Shaik Altaf <sdaltaf7868@gmail.com>
2025-07-28 13:22:03 +0530

    Fix: Add CORS headers for Vercel

--- first.js
+++ first.js
@@ -3,7 +3,12 @@ const app = express();
 const cors = require('cors');
 const jobs = require('./jobsdata'); // 
 const mongoose = require('mongoose');
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or restrict to your frontend domain
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json());
 // Route to get all jobs
