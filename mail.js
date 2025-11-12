b737c53b98a7cd32a3a782121504ced57206365a
Shaik Altaf <sdaltaf7868@gmail.com>
2025-11-12 12:11:00 +0530

    Clean package.json for static deployment

--- mail.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String, // (In production: always hash this)
-});
-
-module.exports = mongoose.model('User', userSchema);
