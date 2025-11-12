9f95f768c100baaaada1034e4ccd00c1b6b101bc
Shaik Altaf <sdaltaf7868@gmail.com>
2025-11-12 12:30:40 +0530

    fix: render deployment crash

--- first.js
+++ first.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jobs = require('./jobdata'); // 
+const jobs = require('./jobsdata'); // 
 app.use(express.json());
 
 
